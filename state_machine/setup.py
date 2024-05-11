3d10b4b06a29d2da94db99fb27d0b834a1c7cdf9
Natalie Huang <natalie.huang99@gmail.com>
2024-05-11 04:39:35 -0400

    setup

--- state_machine/setup.py
+++ state_machine/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import glob
+import os
 
 package_name = 'state_machine'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/state_machine/launch', glob.glob(os.path.join('launch', '*launch.xml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
