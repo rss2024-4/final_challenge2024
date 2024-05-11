08f9498c3366ecae72d1cdb7e213f4cb7ae321e7
Harmanpreet <hpk328@gmail.com>
2024-05-11 07:03:15 +0100

    ahhh merging

--- stop_detector/stop_detector/stop_detector.py
+++ stop_detector/stop_detector/stop_detector.py
@@ -152,7 +152,6 @@ def read_image(path):
 
 def crop_to_bounding(img, bounding_box):
     minx, miny, maxx, maxy = [int(x) for x in bounding_box]
-    minx, miny, maxx, maxy = [int(x) for x in bounding_box]
     return img[miny:maxy, minx:maxx]
 
 # splits image in half horizontally
@@ -171,21 +170,14 @@ def red_percentage(img, node):
     diff = 15
 
     # 'shades' of red to find; loaded in BGR
-    # boundaries = [([green[2], green[1]-diff, green[0]-diff],
-    #        [green[2]+diff, green[1]+diff, green[0]+diff])]
+    boundaries = [([red[2], red[1], red[0]-diff],
+           [red[2]+diff, red[1]+diff, red[0]])]
     
+    for (lower, upper) in boundaries:
+        lower = np.array(lower, dtype=np.uint8)
+        upper = np.array(upper, dtype=np.uint8)
 
-    
-    # for (lower, upper) in boundaries:
-    #     lower = np.array(lower, dtype=np.uint8)
-    #     upper = np.array(upper, dtype=np.uint8)
-
-    lower = np.array([60, 85, 0])
-    upper = np.array([90, 255, 255])
-
-    hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    mask = cv2.inRange(hsvImage, lower, upper)
+        mask = cv2.inRange(img, lower, upper)
 
         ratio_red = cv2.countNonZero(mask)/(img.size/3)
         redness = np.round(ratio_red, 2)
